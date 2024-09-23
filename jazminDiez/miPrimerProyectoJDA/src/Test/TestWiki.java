2b58f7022aa3527637da5d31539cd1a1b7020347
Jazmin10963 <jdiezalmansa@gmail.com>
2024-09-23 10:01:53 -0300

    Agregando los pasos a TestWiki

--- jazminDiez/miPrimerProyectoJDA/src/Test/TestWiki.java
+++ jazminDiez/miPrimerProyectoJDA/src/Test/TestWiki.java
@@ -27,13 +27,19 @@ public class TestWiki {
 	@Test(description = "Validar que las busquedas en Wikipedia funcionan")
 	public void ValidarBusquedaWikipedia() throws Exception {
 		driver.get("http://wikipedia.org");
+		MetodosUtiles.Utiles.step("Abrimos la pagina http://wikipedia.org");
 		WebElement searchInput = driver.findElement(By.id("searchInput"));
+		
 		Assert.assertTrue(searchInput.isDisplayed());
+		MetodosUtiles.Utiles.step("Buscamos el campo de busqueda y chequeamos que esté visible");
+		
 		searchInput.sendKeys("Selenium");
 		searchInput.submit();
+		MetodosUtiles.Utiles.step("Buscamos 'Selenium'");
+		
 		WebElement tituloResultado = driver.findElement(By.id("firstHeading"));
 		System.out.println("Texto encontrado " + tituloResultado.getText());
-		Assert.assertTrue(tituloResultado.isDisplayed());
+		Assert.assertTrue(tituloResultado.isDisplayed(),"No se encontró el título de la página");
 		driver.close();
 	}
 }
