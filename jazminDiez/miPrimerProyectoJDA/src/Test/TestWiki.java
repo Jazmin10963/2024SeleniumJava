ad0b3a69de03a89a86f25c1a2433da5e54f2a523
Jazmin10963 <jdiezalmansa@gmail.com>
2024-09-23 09:54:33 -0300

    Modificando Wiki y Google

--- jazminDiez/miPrimerProyectoJDA/src/Test/TestWiki.java
+++ jazminDiez/miPrimerProyectoJDA/src/Test/TestWiki.java
@@ -4,13 +4,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestWiki {
+	
+	private WebDriver driver;
+	
+	@BeforeMethod
+	public void Precondicion() {
+		// DRIVER CHROME
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@AfterMethod
+	public void PostCondicion() {
+		driver.manage().deleteAllCookies();
+		driver.close();
+	}
 	@Test(description = "Validar que las busquedas en Wikipedia funcionan")
 	public void ValidarBusquedaWikipedia() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
 		driver.get("http://wikipedia.org");
 		WebElement searchInput = driver.findElement(By.id("searchInput"));
 		Assert.assertTrue(searchInput.isDisplayed());
--- jazminDiez/miPrimerProyectoJDA/src/Test/testGoogle.java
+++ jazminDiez/miPrimerProyectoJDA/src/Test/testGoogle.java
@@ -3,30 +3,49 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 import java.util.List;
 
 public class testGoogle {
+	private WebDriver driver;
+	
+	@BeforeMethod
+	public void Precondicion() {
+		// DRIVER CHROME
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@AfterMethod
+	public void PostCondicion() {
+		driver.manage().deleteAllCookies();
+		driver.close();
+	}
 	@Test(description = "Validar que las busquedas en Google funcionan")
 	public void ValidarBusquedaGoogle() throws Exception {
-		//configurar web driver de chrome
-		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
 		
 		//Abrir Google
 		driver.get("https://www.google.com.ar/");
-		
+		MetodosUtiles.Utiles.step("Abrir Google");
 		//Buscar "Selenium"
 		WebElement searchInput = driver.findElement(By.id("APjFqb"));
+		MetodosUtiles.Utiles.step("Encontrando el campo de busqueda");
 		Assert.assertTrue(searchInput.isDisplayed());
 		String busqueda="Java"; 
 		searchInput.sendKeys(busqueda);
 		searchInput.submit();
+		MetodosUtiles.Utiles.step("Buscando: "+busqueda);
 		
 		Thread.sleep(3000);
 		
 		List<WebElement> Resultados = driver.findElements(By.xpath("//h3[contains(text(),'"+busqueda+"')]"));
+		MetodosUtiles.Utiles.step("Guardando resultados de búsqueda");
 		
 		WebElement primerResultado = Resultados.get(0);
 		WebElement tercerResultado = Resultados.get(2);
